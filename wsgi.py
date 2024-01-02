dcafd3c7aba71c4f13e283bd6b61400a2807dfc8
RashLavash <rashlavash000@gmail.com>
2024-01-02 23:40:58 +0300

    some's fix

--- wsgi.py
+++ wsgi.py
@@ -3,4 +3,4 @@ from finances import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=8001)
+    app.run(debug=True, host='0.0.0.0', port=8001)
